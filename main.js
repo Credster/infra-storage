237e1bdc2687eda965c9f350452a8fb0ae6ec583
Liran Tal <liran.tal@gmail.com>
2022-12-19 12:48:06 +0200

    fix: replace hardcoded port with configuration

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const start = async () => {
   const appServer = await server();
 
   try {
-    await appServer.listen({ port: 3001 });
+    await appServer.listen({ port: appServer.config.PORT });
   } catch (err) {
     appServer.log.error(err);
     process.exit(1);
