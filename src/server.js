ea04a19e3ac5ec0c99b48f572788a254a595d1ba
Liran Tal <liran.tal@gmail.com>
2022-12-19 12:23:07 +0200

    feat: add configuration for env variables to app

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ import Fastify from "fastify";
 
 import healthRoutes from "./modules/health/routes/health.routes.js";
 import shutdownPlugin from "./plugins/shutdown.js";
+import configPlugin from "./plugins/config.js";
 
 export async function server(options = { logger: { level: "trace" } }) {
   const fastify = Fastify(options);
 
+  fastify.register(configPlugin);
   fastify.register(fp(shutdownPlugin));
   fastify.register(fp(healthRoutes));
 
