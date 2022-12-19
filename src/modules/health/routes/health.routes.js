e6060168e00884ea0c50a391b531bdb5b6a63390
Liran Tal <liran.tal@gmail.com>
2022-12-19 10:46:09 +0200

    refactor: push health to its own route

---
+++ src/modules/health/routes/health.routes.js
@@ -0,0 +1,5 @@
+export default async function route(server, options) {
+    server.get('/status', async (req, rep) => {
+        return { 'status': true};
+    });
+}
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import fp from 'fastify-plugin';
 import Fastify from 'fastify';
 
+import healthRoutes from './modules/health/routes/health.routes.js';
+
 export async function server (options = { logger: { level: 'trace' }}) {
     const fastify = Fastify(options);
 
-    fastify.get('/status', async (req, rep) => {
-        return { 'status': true}
-    })
+    fastify.register(fp(healthRoutes));
 
-    return fastify
+    return fastify;
 }
