3be8fc886eeea22e3e46ced4e306b2e8f517c75a
Liran Tal <liran.tal@gmail.com>
2022-12-20 00:52:06 +0200

    refactor: out with manual plugin registration and in with fastify autoload for automatically configuring and registering plugins and routes

--- src/server.js
+++ src/server.js
@@ -1,16 +1,26 @@
-import fp from "fastify-plugin";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+import autoload from "@fastify/autoload";
 import Fastify from "fastify";
 
-import healthRoutes from "./modules/health/routes/health.routes.js";
-import shutdownPlugin from "./plugins/shutdown.js";
-import configPlugin from "./plugins/config.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export async function server(options = { logger: { level: "trace" } }) {
   const fastify = Fastify(options);
 
-  fastify.register(fp(configPlugin));
-  fastify.register(fp(shutdownPlugin));
-  fastify.register(fp(healthRoutes));
+  fastify.register(autoload, {
+    dir: join(__dirname, "modules"),
+    dirNameRoutePrefix: true,
+    indexPattern: /.*\.routes(\.ts|\.js|\.cjs|\.mjs)$/,
+  });
+
+  fastify.register(autoload, {
+    dir: join(__dirname, "plugins"),
+    encapsulate: false,
+    ignorePattern: /.*(test|spec).js/,
+  });
 
   return fastify;
 }
