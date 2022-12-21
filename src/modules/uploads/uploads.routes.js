1be93bafd7121943e61c9ac4647e904498259668
Liran Tal <liran.tal@gmail.com>
2022-12-21 10:42:07 +0200

    feat: add a route to generate presigned upload URLs via PUT HTTP

--- src/modules/uploads/uploads.routes.js
+++ src/modules/uploads/uploads.routes.js
@@ -72,6 +72,32 @@ export default async function routesUpload(server, options) {
   });
 
   server.post("/url_token", async (request, reply) => {
-    reply.code(200).send();
+    const bucketName = server.config.GCLOUD_STORAGE_UPLOADS_BUCKET;
+    // @TODO use a fixed identifier for the user such as a
+    // uuid+hash(useremail)+nameOfFileTarget, i.e:
+    // 17172181791212-dvb8b8bv83b+certImage.jpg
+    const fileName = "upload-file.jpg";
+
+    const storageBucket = server.storage.bucket(bucketName);
+    const storageFile = storageBucket.file(fileName);
+
+    const tokenExpirationTime =
+      Date.now() +
+      server.config.GCLOUD_STORAGE_UPLOADS_PRESIGNED_TOKEN_EXPIRATION_MIN *
+        60 *
+        1000;
+    const tokenOptions = {
+      expires: tokenExpirationTime,
+    };
+
+    // The following generated URL can be used to send PUT HTTP
+    // requests to upload files to, such as:
+    //
+    // "curl -X PUT -H 'Content-Type: application/octet-stream' " +
+    //  `--upload-file my-file '${uploadURLToken}'`
+    //
+    const [uploadURLToken] = storageFile.getSignedUrl(tokenOptions);
+
+    reply.code(200).send({ uploadURLToken });
   });
 }
