db954551c6ffe94a62738d5b15a34b35193691f7
Liran Tal <liran.tal@gmail.com>
2022-12-21 10:38:33 +0200

    feat: add route to handle HTML form rendering

--- src/modules/uploads/uploads.routes.js
+++ src/modules/uploads/uploads.routes.js
@@ -25,6 +25,52 @@ export default async function routesUpload(server, options) {
     reply.code(200).send(response);
   });
 
+  // @TODO move all of this token handling
+  // out to a service to not duplicate code
+  // ---
+  // @TODO this shouldn't be exposed publicly
+  // and definitely not without a ratelimit
+  // in place to avoid spamming the cloud storage
+  // service APIs
+  server.get("/form", async (request, reply) => {
+    const bucketName = server.config.GCLOUD_STORAGE_UPLOADS_BUCKET;
+    // @TODO use a fixed identifier for the user such as a
+    // uuid+hash(useremail)+nameOfFileTarget, i.e:
+    // 17172181791212-dvb8b8bv83b+certImage.jpg
+    const fileName = "upload-file.jpg";
+
+    const storageBucket = server.storage.bucket(bucketName);
+    const storageFile = storageBucket.file(fileName);
+
+    const tokenExpirationTime =
+      Date.now() +
+      server.config.GCLOUD_STORAGE_UPLOADS_PRESIGNED_TOKEN_EXPIRATION_MIN *
+        60 *
+        1000;
+    const tokenOptions = {
+      expires: tokenExpirationTime,
+    };
+
+    const [response] = await storageFile.generateSignedPostPolicyV4(
+      tokenOptions
+    );
+    // ---
+
+    // Construct an HTML form with the provided policy
+    let output = `<form action="${response.url}" method="POST" enctype="multipart/form-data">\n`;
+    // Include all fields returned in the HTML form as they're required
+    for (const name of Object.keys(response.fields)) {
+      const value = response.fields[name];
+      output += `  <input name="${name}" value="${value}" type="hidden"/>\n`;
+    }
+    output += '  <input type="file" name="file"/><br />\n';
+    output += '  <input type="submit" value="Upload File"/><br />\n';
+    output += "</form>";
+
+    reply.type("text/html");
+    reply.send(output);
+  });
+
   server.post("/url_token", async (request, reply) => {
     reply.code(200).send();
   });
